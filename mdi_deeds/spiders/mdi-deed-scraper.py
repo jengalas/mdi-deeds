5c23413a6315aadd670be04f19a71890adb34b06
Jennifer Galas <jennifer.galas@scranton.edu>
2025-03-25 21:06:17 -0400

    Update deeds after website update

--- mdi_deeds/spiders/mdi-deed-scraper.py
+++ mdi_deeds/spiders/mdi-deed-scraper.py
@@ -6,14 +6,18 @@ import pathlib
 
 class MdiDeedSpider(scrapy.Spider):
     name = 'mdideedspider'
-    allowed_domains = ['mdihistory.org']
-    start_urls = ['http://mdihistory.org/Cultural_History_Project/htdocs/MDIdeeds']
-    pathlib.Path('deeds').mkdir(parents=True, exist_ok=True)
+    allowed_domains = ['vfthomas.com']
+
+    def start_requests(self):
+        start_urls = ['http://www.vfthomas.com/MDI%20Cultural%20History%20Project/MDIdeedshome.htm']
+        pathlib.Path('deeds').mkdir(parents=True, exist_ok=True)
+        for url in start_urls:
+            yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
         links = response.xpath("//body//a/@href").extract()
         for link in links[1:]:
-            url = 'http://mdihistory.org/Cultural_History_Project/htdocs/MDIdeeds/' + link
+            url = 'http://www.vfthomas.com/MDI%20Cultural%20History%20Project/' + link
             yield Request(url=url, callback = self.parse_item)
 
     def parse_item(self, response):   
